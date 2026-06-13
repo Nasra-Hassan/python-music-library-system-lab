fe95f2dbadb59d711a7954c542f1792c43735d59
Nasra <nasra.hassan@student.moringaschool.com>
2026-06-13 18:29:11 +0300

    Implement Song class with class attributes and counters

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,25 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        Song.genre_count.setdefault(genre, 0)
+        Song.genre_count[genre] += 1
+
+        Song.artist_count.setdefault(artist, 0)
+        Song.artist_count[artist] += 1
